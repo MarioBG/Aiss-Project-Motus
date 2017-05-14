0871f920ab05b6b0d790ab9e658243a12ffca9fd
HisperianHub <mario.biz.gon@gmail.com>
2017-05-15 01:16:41 +0200

    Changes

--- src/main/java/aiss/api/ConsorcioComments.java
+++ src/main/java/aiss/api/ConsorcioComments.java
@@ -1,7 +1,36 @@
 package aiss.api;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
 
-public class ConsorcioComments extends Application {
+import aiss.model.resource.ConsorcioResource;
 
+public class ConsorcioComments extends Application {
+	
+	private Set<Object> singletons = new HashSet<Object>();
+	private Set<Class<?>> classes = new HashSet<Class<?>>();
+	
+	public ConsorcioComments(){
+		singletons.add(ConsorcioResource.class);
+	}
+
+	public Set<Class<?>> getClasses(){
+		return classes;
+	}
+	
+	public Set<Object> getSingletons(){
+		return singletons;
+	}
+	
+	@GET
+	@Produces("application/json")
+	public String getCosicas(){
+		return "meh";
+	}
+	
 }
